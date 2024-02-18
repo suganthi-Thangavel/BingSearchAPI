a5ce433cfd998f1991ab1a8ea1c1e7ef91f3bd99
suganthi <suganthi.th@gmail.com>
2024-02-18 13:47:43 +0530

    Added files

---
+++ bing-search/src/App.js
@@ -0,0 +1,12 @@
+import BingSearchComponent from './bing-search-search-componnent'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App with Custom Search</h1>
+      <BingSearchComponent />   
+    </div>
+  );
+}
+
+export default App;
